38bc97303f93e74d061e3b83d534b20786b0f92e
willbraun <williamhbraun1@gmail.com>
2026-03-20 08:27:41 -0400

    finding fen div

--- copy-fen.js
+++ copy-fen.js
@@ -1,7 +1,7 @@
 javascript: (async () => {
 	try {
-		const el = document.querySelector('.analysis-view-lines')
-		if (!el) throw new Error('Element .analysis-view-lines not found')
+		const el = document.querySelector('div[fen]')
+		if (!el) throw new Error('Div with fen attribute not found')
 
 		const fen = el.getAttribute('fen')
 		if (!fen) throw new Error('FEN attribute not found')
